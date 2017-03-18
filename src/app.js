40db60bc885950e9ad67b6aecab7074bed5e513b
carpie <elcarpie@gmail.com>
2017-03-18 17:22:09 -0400

    Added /requests resource handler

--- src/app.js
+++ src/app.js
@@ -3,10 +3,17 @@
 const express = require('express');
 const app = express();
 
+const syslogMonitor = require('./syslogmonitor.js');
+
 app.use('/$', (req, res) => {
   res.send('Hello world.');
 });
 
+app.get('/requests$', (req, res) => {
+  res.json(syslogMonitor.getRequests());
+});
+
 app.listen(3400, () => {
   console.log('SID listening on port 3400...');
+  syslogMonitor.monitor();
 });
